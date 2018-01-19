ac72a469996e8cf7bbb92e45807bd4b08829b80a
justinmlyle <justin.m.lyle@gmail.com>
2018-01-19 16:18:31 -0600

    tweaks and bug fixes

---
+++ portfolio/js/webscripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+
+    $("#u4emodal").on('hidden.bs.modal', function (e) {
+        $("#u4emodal iframe").attr("src", $("#u4emodal iframe").attr("src"));
+    });
+
+    $("#dkmodal").on('hidden.bs.modal', function (e) {
+        $("#dkmodal iframe").attr("src", $("#dkmodal iframe").attr("src"));
+    });
+
+    $("#tstomodal").on('hidden.bs.modal', function (e) {
+        $("#tstomodal iframe").attr("src", $("#tstomodal iframe").attr("src"));
+    });
+
+  
+   
+                
+   
+});
