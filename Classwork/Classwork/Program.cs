37add72058289aceefcf40a560c4a84955533ad6
Justin Lyle <justin.m.lyle@gmail.com>
2018-01-19 14:38:46 -0600

    new assets

---
+++ Classwork/Classwork/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Classwork
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+
+        {
+            int yards = 0;
+            Console.WriteLine("How many Yards do you want to convert?");
+            string yardInput = Console.ReadLine();
+            int.TryParse(yardInput, out yards);
+            yards = yards * 36;
+
+
+            Console.WriteLine(yardInput + " yards is equal to " + yards + " inches");
+
+
+
+        }
+    }
+}
